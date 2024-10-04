7d53f757294a0c53d06a27fed46ed8f51cea6b2e
Fernando Chiñas <A00832747@tec.mx>
2024-10-04 13:52:41 -0600

    prueba temporal vista user

---
+++ SKILIKET Project Team 7/Controllers/UsersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UsersViewController.swift
+//  SKILIKET Project Team 7
+//
+//  Created by Fernando Chiñas on 04/10/24.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SKILIKET Project Team 7/Models/userJSON.swift
@@ -0,0 +1,8 @@
+//
+//  userJSON.swift
+//  SKILIKET Project Team 7
+//
+//  Created by Fernando Chiñas on 04/10/24.
+//
+
+import Foundation
