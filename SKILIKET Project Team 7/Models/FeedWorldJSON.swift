ffc45332d99ca1275b9320b9cc6ce9b820ef85a4
Fernando Chiñas <A00832747@tec.mx>
2024-10-06 06:10:05 -0600

    Agregar vista de feed general ( no local )

---
+++ SKILIKET Project Team 7/Models/FeedWorldJSON.swift
@@ -0,0 +1,8 @@
+//
+//  FeedWorldJSON.swift
+//  SKILIKET Project Team 7
+//
+//  Created by Fernando Chiñas on 06/10/24.
+//
+
+import Foundation
---
+++ SKILIKET Project Team 7/Models/worldFeedJSON.swift
@@ -0,0 +1,8 @@
+//
+//  worldFeedJSON.swift
+//  SKILIKET Project Team 7
+//
+//  Created by Fernando Chiñas on 06/10/24.
+//
+
+import Foundation
